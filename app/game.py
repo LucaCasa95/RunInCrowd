f5a8bcf474918194fe427ddf362d1a5e80a08688
LucaCasa95 <lucacasa95@gmail.com>
2023-11-13 22:06:27 +0100

    scrollable background

--- app/game.py
+++ app/game.py
@@ -1,25 +1,25 @@
-import pyxel
-import game_config
+from pyxel import init, run, cls
+from game_config import WINDOW_WIDTH, WINDOW_HEIGHT, BACKGROUND_SCROLL_SPEED
 from game_loop.draw_objects import DrawObjects
 from game_loop.update_objects import UpdateObjects
 
 class App:
     def __init__(self):
-        init_game()
-        pyxel.init(game_config.WINDOW_WIDTH, game_config.WINDOW_HEIGHT)
-        pyxel.run(self.update, self.draw)
+        init(WINDOW_WIDTH, WINDOW_HEIGHT)
+        init_game(self)
+        run(self.update, self.draw)
 
     def update(self):
-        #UpdateObjects(self.player1)
+        UpdateObjects(self.background)
         pass
 
     def draw(self):
-        pyxel.cls(0)
-        #DrawObjects(self.player)
+        cls(0)
+        DrawObjects(self.background)
 
-def init_game():
-    #create gamne objects
-    pass
+def init_game(self):
+    from environment.background import Background
+    self.background = Background("assets/background.png", BACKGROUND_SCROLL_SPEED)
 
 if __name__ == "__main__":
     App()
